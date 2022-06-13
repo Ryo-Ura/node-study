278e94da62554944d046e1249ef921cfce42a17d
Ryo-Ura <urawaryosuke@gmail.com>
2022-06-13 23:59:04 +0900

    setted page status for 404

--- Node_creating_server/app.js
+++ Node_creating_server/app.js
@@ -15,7 +15,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use((req, res,next)=>{
     // res.status(404).send('<h1>Page not found</h1>');
-    res.sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 
