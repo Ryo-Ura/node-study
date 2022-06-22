013a2f10b31a022d90714a6b4537582584d3abfd
Ryo-Ura <urawaryosuke@gmail.com>
2022-06-22 22:45:43 +0900

    fixed the bug of not highlighting header menu

--- Node_creating_server/routes/admin.js
+++ Node_creating_server/routes/admin.js
@@ -7,7 +7,7 @@ const products = [];
 router.get('/add-product', (req, res, next)=>{
     console.log('in the other middleware');
     // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">add product</button></input></form>');
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 });
 
 router.post('/add-product',(req, res, next)=>{
