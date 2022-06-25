cc6d6378fd6d7d13f1c599208f872f51a98a70be
Ryo-Ura <urawaryosuke@gmail.com>
2022-06-25 15:05:20 +0900

    refactor existing ejs files

--- Node_creating_server/app.js
+++ Node_creating_server/app.js
@@ -18,7 +18,13 @@ app.use(shopRoutes);
 app.use((req, res,next)=>{
     // res.status(404).send('<h1>Page not found</h1>');
     // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404', { pageTitle: '404' });
+    res.status(404).render('404', {
+        pageTitle: '404',
+        path: '',
+        formCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+    });
 })
 
 
